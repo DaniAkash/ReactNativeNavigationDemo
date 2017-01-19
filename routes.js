19ba684e76200350b002048284327111964a5512
DaniAkash <s.daniakash@gmail.com>
2017-01-19 13:46:42 +0530

    Routes Data

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 export default [
   {title: 'Splash Screen', index: -1},
   {title: 'Home Page', index: 0},
-  {title: 'First Page', index: 1},
-  {title: 'Second Page', index: 2},
-  {title: 'Third Page', index: 3}
+  {title: 'First Page', index: 1, data:{}},
+  {title: 'Second Page', index: 2, data: {}},
+  {title: 'Third Page', index: 3, data: {}}
 ];
